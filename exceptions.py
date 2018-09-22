d5f22a7068787ea8773b87131b5c474da52e0147
Biagio Distefano <biagiodistefano92@gmail.com>
2018-09-22 14:57:41 +0200

    added exceptions and field_types. Renamed main to sqliter

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+class NoSuchTable(Exception):
+    pass
+
+
+class CannotAccessProtectedField(Exception):
+    pass
+
+
+class InvalidFieldName(Exception):
+    pass
+
+
+class NoSuchEntry(Exception):
+    pass
+
+
+class NoSuchField(Exception):
+    pass
